53f3c06856777e5ddc98cf83fbc153cfb0da243c
Muhammad Rizwan <muhammad.rizwan@nmxglobalsoftware.com>
2019-05-09 16:58:23 +0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -41,7 +41,7 @@ async function getENSContractEvents(fromBlockNumber, toBlockNumber) {
  */
 export default async function getEventsForPast2Days() {
   let latestBlockNumber = await getLatestBlockNumber();
-  let twoDaysOldBlockNumber = latestBlockNumber - 5000;
+  let twoDaysOldBlockNumber = latestBlockNumber - 10000;
   let ensContractEvents = await getENSContractEvents(
     twoDaysOldBlockNumber,
     latestBlockNumber
